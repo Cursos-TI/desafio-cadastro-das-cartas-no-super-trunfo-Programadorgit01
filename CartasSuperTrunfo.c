db21983729a06ef713282361099429f5024995c1
Programadorgit01 <Lucasdosbonifacio@gmail.com>
2025-09-02 03:39:59 +0000

    criei um printf

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -4,7 +4,7 @@
 
         char estado [20]= "Ceara";
         char cidade [50]= "fortaleza";
-        char codigo [20]= "A01";
+        char código [20]= "A01";
         int população = 9.233;
         float area = 312.353;
         float PIB = 70.000;
@@ -16,10 +16,21 @@
         printf("digite a cidade: \n");
         scanf("%s", &cidade);
 
-        printf("digite o codigo: \n");
-        scanf("%s, &codigo");
+        printf("digite o código da carta: \n");
+        scanf("%s", &código);
+        
+        printf("digite a população: \n");
+        scanf("%d", &população);
 
-        printf(" ")
+        printf("digite a area: \n");
+        scanf("%f", &area);
+
+        printf("digite o PIB: \n");
+        scanf("%f", &PIB);
+
+        printf("digite os pontos turisticos: \n");
+        scanf("%d", &pontos);
+
 
 
 
